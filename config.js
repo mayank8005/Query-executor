6112d74736a5668ace9b3f337791ee16a8fea0cd
mayank <mayank8005@gmail.com>
2022-06-18 23:42:55 +0530

    add queries mapping

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 const config = {
-    headerText: 'Query Executor'
+    headerText: 'Query Executor',
+    queryMap: {
+        'select * from small-table': 'public/mockQueriesData/small-table.json',
+        'select * from large-table': 'public/mockQueriesData/large-table.json',
+        'select * from xl-table': 'public/mockQueriesData/xl-table.json'
+    }
 }
 
 export default config;
