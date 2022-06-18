aad3466c418dcadebf8cd759e00291c1b0e2f613
mayank <mayank8005@gmail.com>
2022-06-19 04:02:28 +0530

    remove debug code

--- components/QueryResult.js
+++ components/QueryResult.js
@@ -1,10 +1,8 @@
-import { useMemo } from 'react'
+import { useMemo } from "react";
 import { AutoSizer, Column, Table } from "react-virtualized";
-import Tooltip from '@mui/material/Tooltip';
+import Tooltip from "@mui/material/Tooltip";
 
 const QueryResult = (props) => {
-
-
   if (props.executingQuery) {
     return (
       <div className="mt-4 w-3/4 p-8 rounded-md bg-sky-300 border-white border-2">
@@ -14,64 +12,76 @@ const QueryResult = (props) => {
   }
 
   const cols = useMemo(() => {
-      if(!props.queryResult || !props.queryResult.length) return [];
-      return props.queryResult[0].map((header) => ({label: header, dataKey: header, width: 200}))
+    if (!props.queryResult || !props.queryResult.length) return [];
+    return props.queryResult[0].map((header) => ({
+      label: header,
+      dataKey: header,
+      width: 200,
+    }));
   }, [props.queryResult]);
 
   const rows = useMemo(() => {
-    if(!props.queryResult || !props.queryResult.length >= 2) return [];
-    console.log(props.queryResult.slice(1).map(row => (row.reduce((acc, curr, idx) => ({...acc, [props.queryResult[0][idx]]: curr}), {}))))
-    return props.queryResult.slice(1).map(row => (row.reduce((acc, curr, idx) => ({...acc, [props.queryResult[0][idx]]: curr}), {})))
-    }, [props.queryResult]);
+    if (!props.queryResult || !props.queryResult.length >= 2) return [];
+    return props.queryResult
+      .slice(1)
+      .map((row) =>
+        row.reduce(
+          (acc, curr, idx) => ({ ...acc, [props.queryResult[0][idx]]: curr }),
+          {}
+        )
+      );
+  }, [props.queryResult]);
 
   const cellRenderer = ({ cellData }) => (
     <div>
-       <Tooltip title={cellData}>
-        <div className="text-black whitespace-nowrap overflow-hidden font-bold text-sm text-ellipsis w-11/12">{cellData}</div>
-       </Tooltip>
+      <Tooltip title={cellData}>
+        <div className="text-black whitespace-nowrap overflow-hidden font-bold text-sm text-ellipsis w-11/12">
+          {cellData}
+        </div>
+      </Tooltip>
     </div>
   );
 
   const headerRenderer = ({ label }) => (
     <div>
-      <span className='text-white font-bold text-sm'>{label}</span>
+      <span className="text-white font-bold text-sm">{label}</span>
     </div>
   );
 
   return (
     <>
-        <div className="mt-4 w-3/4 rounded-md bg-sky-300 border-white border-2 grow">
+      <div className="mt-4 w-3/4 rounded-md bg-sky-300 border-white border-2 grow">
         <AutoSizer>
-            {({ height, width }) => (
+          {({ height, width }) => (
             <Table
-                height={height}
-                width={width}
-                rowHeight={25}
-                headerHeight={25}
-                rowCount={rows.length}
-                rowGetter={({ index }) => rows[index]}
+              height={height}
+              width={width}
+              rowHeight={25}
+              headerHeight={25}
+              rowCount={rows.length}
+              rowGetter={({ index }) => rows[index]}
             >
-                {cols.map(({ dataKey, label }) => {
+              {cols.map(({ dataKey, label }) => {
                 return (
-                    <Column
+                  <Column
                     key={dataKey}
                     width={300}
                     headerRenderer={(headerProps) =>
-                        headerRenderer({...headerProps})
+                      headerRenderer({ ...headerProps })
                     }
                     cellRenderer={cellRenderer}
                     dataKey={dataKey}
                     label={label}
-                    />
+                  />
                 );
-                })}
+              })}
             </Table>
-            )}
+          )}
         </AutoSizer>
-        </div>
-        <div className='w-3/4 my-4 flex text-white font-bold'>
-            Row count: {rows.length}
-        </div>
+      </div>
+      <div className="w-3/4 my-4 flex text-white font-bold">
+        Row count: {rows.length}
+      </div>
     </>
   );
 };
